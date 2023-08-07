12588a069fbde9ada474a0bf88cf6243794f9fb4
Oluwatobi Adetunji <Oluwatobi Adetunji>
2023-08-07 09:49:09 -0700

    updated

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -6,9 +6,8 @@ void _puts(char *str)
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
